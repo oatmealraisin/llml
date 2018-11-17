9fb6dbce90c5af15db2f3f28bd98b1ee48c1a0c2
oatmealraisin <rymurphy@redhat.com>
2018-11-17 12:35:00 -0500

    Modified format of license boilerplate

--- datasets/mnist/idx.c
+++ datasets/mnist/idx.c
@@ -1,23 +1,25 @@
-/* idx.c: Utility functionf for the IDX file format
-   Copyright (C) 2018  J. Ryan Murphy <murphy2902@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
-
-/* idx.c
- * For working with IDX style files, particularly the MNIST dataset.
+/*
+ * idx.c: Utility functionf for the IDX file format
  * http://yann.lecun.com/exdb/mnist/
+ * 
+ * Copyright (C) 2018 oatmealraisin (ryan@murph.ai)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+#ifndef llml_idx_c
+#define llml_idx_c
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -212,3 +214,4 @@ void *getAllSamples(FILE *idxFile) {
 
 	return data;
 }
+#endif
